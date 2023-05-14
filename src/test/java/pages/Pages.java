4a4eded1fd0348ae09dc6d276b34cc9361743be0
Darina <dturtseva@gmail.com>
2023-05-14 08:17:46 +0700

    change properties

--- src/test/java/pages/Pages.java
+++ src/test/java/pages/Pages.java
@@ -8,8 +8,14 @@ import pages.login.*;
 public final class Pages {
     @Step("Открыть страницу с формой входа в Систему")
     public static UserGateAuthPage openAuthPage() {
-        Selenide.open(Project.config.baseUrl());
-        return new UserGateAuthPage();
+        try {
+            System.err.println("######## OPEN " + Project.config.baseUrl());
+            Selenide.open(Project.config.baseUrl());
+            return new UserGateAuthPage();
+        }catch(RuntimeException e){
+            e.printStackTrace();
+            throw e;
+        }
     }
 
 }
